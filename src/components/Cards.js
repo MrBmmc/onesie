8361502581df90cc41f0830c8cca854d6f4cf1eb
Brian McMullen <clanmcmullen2012@gmail.com>
2022-09-21 13:46:45 -0400

    building card display

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,91 +1,15 @@
 import React from "react";
+import Deck from "Deck.js";
 
-export const cards = [
-    {
-        id: 0,
-        number: 0,
-    },
-    {
-        id: 1, 
-        number: 1,
-    },
-    {
-        id: 2, 
-        number: 2,
-    },
-    {
-        id: 3, 
-        number: 3,
-    },
-    {
-        id: 4, 
-        number: 4,
-    },
-    {
-        id: 5, 
-        number: 5,
-    },
-    {
-        id: 6, 
-        number: 6,
-    },
-    {
-        id: 7, 
-        number: 7,
-    },
-    {
-        id: 8, 
-        number: 8,
-    },
-    {
-        id: 9, 
-        number: 9,
-    },
-    {
-        id: 10, 
-        name: doubleDraw,
-        number: 10,
-    },
-    {
-        id: 11, 
-        name: block,
-        number: 11,
-    },
-    {
-        id: 12,
-        name: reverse, 
-        number: 12,
-    },
-];
-
-export const colors = [
-    {
-        id: 0,
-        color: "red",
-    },
-    {
-        id: 1,
-        color: "blue",
-    },
-    {
-        id: 2,
-        color: "yellow",
-    },
-    {
-        id: 3,
-        color: "green",
-    },
-];
-
-export const specialCard = [
-    {
-            id: 13, 
-            name: wild,
-            number: 13,
-        },
-        {
-            id: 14,
-            name: quadDraw,
-            number: 14,
-        },
-];
+const Cards = ({ id,color,number, name}) => {
+    return (
+        <div className="cardContainer">
+            <div className = {'card-container_inner ${name}'}>
+                <div className = {'card-container-inner_border ${color}'}>
+                    <p>{number}</p>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
